5e99bb861b8696733bc20a7249825368483e80a2
MarguoNana <n.marguerite@live.fr>
2020-01-07 23:45:56 +0100

    Public router

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor( router: Router) { }
 
   ngOnInit() {
   }
