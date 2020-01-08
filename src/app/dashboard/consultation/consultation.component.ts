835c978b837eadfa986b9a45936ae6bdf6b790b8
MarguoNana <n.marguerite@live.fr>
2020-01-08 18:35:21 +0100

    Check message chaque 1seconde au lieu de 1ms

--- src/app/dashboard/consultation/consultation.component.ts
+++ src/app/dashboard/consultation/consultation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, DoCheck } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, DoCheck, OnDestroy } from '@angular/core';
 import { ApiService } from '@src/app/service/api.service';
 import { HttpParams } from '@angular/common/http';
 import { Observable, interval } from 'rxjs';
@@ -10,7 +10,7 @@ import { map, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./consultation.component.scss'],
   changeDetection : ChangeDetectionStrategy.OnPush
 })
-export class ConsultationComponent implements OnInit, DoCheck {
+export class ConsultationComponent implements OnInit, OnDestroy {
 
   /**
    * Prochainement: Array<object> = { 'nom': '', msg: ''}
@@ -22,31 +22,40 @@ export class ConsultationComponent implements OnInit, DoCheck {
 
   constructor(private api : ApiService) { }
 
-  ngOnInit() {   
-  }
-
-  ngDoCheck(): void {
-      this.api.getUnhandled<Array<string>>('message/').toPromise()
-      .then(received => {
-        if(received && received.length > 0){
-          this.messageList = received;
-          if (this.messageList && this.messageList.length < 0){
-            this.aucunMsg = true;
-          }else{
-            this.aucunMsg = false;
-          }
-        }
-      })
-      .catch( err => this.aucunMsg = true) 
+  ngOnInit() {  
+    this.observableMsgList = Observable.create((observer : any) => {
+      try {
+        setInterval(() => {
+          observer.next(this.checkMessage())
+        },1000)
+      } catch (err){
+        observer.error(err);
+      }
+    }) 
+    this.observableMsgList.subscribe();
   }
 
   checkMessage() {
-
-    
+    this.api.getUnhandled<Array<string>>('message/').toPromise()
+    .then(received => {
+      if(received && received.length > 0){
+        this.messageList = received;
+        if (this.messageList && this.messageList.length < 0){
+          this.aucunMsg = true;
+        }else{
+          this.aucunMsg = false;
+        }
+      }
+    })
+    .catch( err => this.aucunMsg = true)   
   }
 
   deleteMsg(text) {
     this.messageList = this.messageList.filter(el => { return el != text});
+  }
+
+  ngOnDestroy() {
+    this.observableMsgList.unsubscribe();
   }
 
 }
